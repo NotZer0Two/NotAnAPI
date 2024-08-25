068a7cc954d3a29a9edb4de2365a234e0f37e4c2
NotZer0Two <lyonfan2010@gmail.com>
2024-08-25 11:38:39 +0200

    First commit

---
+++ NotAnAPI/API/Extensions/HashsetExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotAnAPI.API.Extensions
+{
+    public static class HashsetExtension
+    {
+
+        public static bool TryGetValue<T>(this HashSet<T> set, T equalValue, out T actualValue)
+        {
+            if (set.Contains(equalValue))
+            {
+                actualValue = equalValue;
+                return true;
+            }
+            actualValue = default;
+            return false;
+        }
+
+    }
+}
---
+++ NotAnAPI/Features/PAPI/API/PlaceholderAPI.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+using Exiled.API.Features;
+using NotAnAPI.API.Extensions;
+using NotAnAPI.Features.PAPI.API.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnityEngine.Windows;
+
+namespace NotAnAPI.Features.PAPI.API
+{
+    public static class PlaceholderAPI
+    {
+
+        private const string pattern = @"%(?<identifier>[a-zA-Z0-9]+)_(?<params>[^%]+)%";
+
+        private static readonly Regex regex = new Regex(pattern);
+
+        public static string SetPlaceholders(Player player, string text)
+        {
+            return regex.Replace(text, match =>
+            {
+                string identifier = match.Groups["identifier"].Value;
+                string parameters = match.Groups["params"].Value;
+
+                if (PAPIFeature.Instance.Placeholders.TryGetValue(identifier, out PlaceholderExpansion replacement))
+                {
+                    string final = replacement.onRequest(player, parameters);
+
+                    return final == null ? "NaN" : final;
+                }
+                else
+                {
+                    return match.Value;
+                }
+            });
+        }
+
+    }
+}
